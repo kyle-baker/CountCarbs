2240fa068efb2456c269d8d08534f57dd3f0ae50
Kyle Baker <kylekb1@gmail.com>
2018-01-27 22:03:00 -0500

    Added ability to create item

---
+++ public/js/create-item.js
@@ -0,0 +1,33 @@
+function registerEvent() {
+  console.log("registerEvent ran");
+  $("#create-form").submit(event => {
+    event.preventDefault();
+    console.log("event listener worked!");
+    let name = $("#item-name").val();
+    let carbs = $("#item-carbs").val();
+    let serving = $("#item-serving").val();
+    let calories = $("#item-calories").val();
+    let fiber = $("#item-fiber").val();
+
+    let payload = {
+      name: name,
+      carbs: carbs,
+      serving: serving,
+      calories: calories,
+      fiber: fiber,
+      publicAccess: true
+    }
+
+    fetch('/create-carb-item', { 
+      method: 'POST',
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)      
+    }).then(res => {
+      return res.json()
+    }).then(response => {
+      console.log(response);
+    })
+  })
+}
+
+$(registerEvent());
